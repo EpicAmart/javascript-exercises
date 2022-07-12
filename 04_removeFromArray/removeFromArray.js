8d5885aa66a7b41858ada13633b7cc5e42169f0e
Amartya Ranganathan <amartyaranganathan@Amartyas-MacBook-Pro.local>
2022-07-11 23:35:43 -0700

    Exercises so far

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers, removal) {
+    for(let i = 0; i < numbers.length; i++){
+        if(numbers[i] == removal){
+            numbers.splice(i,1);
+        }
+    }
+    console.log(numbers);
+    return numbers;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,10 @@
-const sumAll = function() {
+const sumAll = function(low, high) {
+    let result = 0;
+    for(let i = low; i <= high; i++){
+        result += i;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
